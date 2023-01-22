85f255fb6f42a393d05e331ad51eb7ba1ebee1ba
wojtek s <wojciech.sowinski@windowslive.com>
2023-01-22 14:29:54 +0100

    added PUT/DELETE requests

--- working.py
+++ working.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query, HTTPException, status
 from typing import Optional
 from pydantic import BaseModel
 
@@ -7,39 +7,61 @@ class Item(BaseModel):
     price: float
     brand: Optional[str] = None
 
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
 
 app = FastAPI() #create API object
 
-inventory = {
-    1: {
-        "name":"Milk",
-        "price":3.99,
-        "brand": "Regular"
-    },
-    2: {
-        "name":"Butter",
-        "price":2.22
-    }
-}
+inventory = {}
 
 
 @app.get("/get-item/{item_id}/{name}")
 def get_item(item_id: int = Path(None,description="The ID of the item you would like to view")): #PATH adds additional information to documentation at /docs
+    if item_id not in inventory:
+        raise HTTPException(status_code=404, detail="Item ID not found.")
     return inventory[item_id]
-
+    
 #QUERY PARAMETERS
-@app.get("/get-by-name/{item_id}")
-def get_item(*, item_id: int, name: Optional[str] = None, test: int): #name: str = None - means it's optional parameter but it's better to use Optional packet from library
+@app.get("/get-by-name")
+def get_item(name: str = Query(None, title="Name", description="Name of item.")):
     for item_id in inventory:
         if inventory[item_id]["name"] == name:
             return inventory[item_id]
-    return {"Data": "Not found"}
+    raise HTTPException(status_code=404, detail="Item name not found.")
 
 
 @app.post("/create-item/{item_id}")
 def create_item(item_id: int, item: Item):
     if item_id in inventory:
-        return {"Error":"Item ID already exists."}
+        raise HTTPException(status_code=400, detail="Item ID already exists.")
     
     inventory[item_id] = {"name": item.name, "brand": item.brand, "price": item.price}
     return inventory[item_id]
+
+
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item: UpdateItem):
+
+    
+    if item_id not in inventory:
+        raise HTTPException(status_code=404, detail="Item ID does not exist")
+    else:
+        if item.name != None:
+            inventory[item_id]["name"] = item.name
+
+        if item.price != None:
+            inventory[item_id]["price"] = item.price
+
+        if item.brand != None:
+            inventory[item_id]["brand"]= item.brand    
+
+    return inventory[item_id]
+
+@app.delete("/delete-item")
+def delete_item(item_id: int = Query(..., description="The ID of the item to delete.", gt=0)):
+    if item_id not in inventory:
+        raise HTTPException(status_code=404, detail="Item ID does not exist")
+    del inventory[item_id]
+    return {"Success":"Item deleted!"}
